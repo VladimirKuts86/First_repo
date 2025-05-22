f094857d389702d27cfa682fa2f5e4477be8e8b4
VladimirKuts86 <kuts_va@bk.ru>
2025-05-22 20:36:20 +0300

    first changes

---
+++ Practice/practice1.py
@@ -0,0 +1,30 @@
+# Задача 1
+
+first = int(input("Введите первое целое число: "))
+two = int(input("введите второе целое число: "))
+result = first + two
+print(result)
+
+# Задача 2
+
+string = input("Введите число ")
+string = int(string)
+print(string)
+print(type(string))
+
+# Задача 3
+
+first = int(input("Введите первое целое число: "))
+two = int(input("введите второе целое число: "))
+result = first - two
+print(result)  
+
+# Задача 4
+
+first = float(input("Введите первое дробное число: "))
+two = float(input("введите второе дробное число: "))
+result_one = first * two
+result_two = first / two
+print("Результат умножения: ", result_one, "Результат деления: ", result_two)
+
+# Задача 5
